92c5dee54ad8ca62355443dd70ab902d38daeb00
Walter Eaves <weaves@artikus.co.uk>
2021-08-19 13:25:06 +0200

    -

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,1 +1,6 @@
-../prog-scala-2nd-ed-code-examples/project/Dependencies.scala
+import sbt._
+
+object Dependencies {
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0" % "test" ;
+  lazy val scalaCheck = "org.scalacheck"         %% "scalacheck"      % "1.14.1" % "test" ;
+}
